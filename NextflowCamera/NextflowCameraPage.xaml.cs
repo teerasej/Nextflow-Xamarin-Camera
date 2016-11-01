0d9237cdde526bfdac6e5e6503f9105cadb5205c
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-01 21:35:53 +0700

    Completed

---
+++ NextflowCamera/NextflowCameraPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Plugin.Media;
+using Xamarin.Forms;
+using Plugin.Media.Abstractions;
+
+namespace NextflowCamera
+{
+	public partial class NextflowCameraPage : ContentPage
+	{
+		public NextflowCameraPage()
+		{
+			InitializeComponent();
+		}
+
+		async void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			await CrossMedia.Current.Initialize();
+
+			var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions { 
+				Directory = "MyPhoto",
+				Name = "Nextflow.jpg",
+				SaveToAlbum = true
+			});
+
+			if (file == null)
+				return;
+
+			image.Source = ImageSource.FromStream(() => {
+				var stream = file.GetStream();
+				file.Dispose();
+				return stream;
+			});
+		}
+	}
+}
